d9f9f6dbbd1d4b1cccc02d9d32032bf98c51f128
SamsonLin <153011490@qq.com>
2019-08-12 20:16:46 +0800

    test:add test given a normal item with sellIn is 10 and quality is 0 then return selleIn is 9 and quality is 0

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -2,14 +2,11 @@ package gildedRose;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-
-import static java.util.Collections.singletonList;
 import static org.junit.Assert.*;
 
 public class GildedRoseTest {
     @Test
-    public void should_return_sellIn_is_9_quality_is_9_when_call_updateQuality_given_a_normal_item_with_sellIn_is_10_quality_is_10() throws Exception {
+    public void should_return_sellIn_is_9_quality_is_9_when_call_updateQuality_given_a_normal_item_with_sellIn_is_10_quality_is_10() {
         Item item = new Item("Oreo", 10, 10);
         Item[] items = {item};
         GildedRose gildedRose = new GildedRose(items);
@@ -19,4 +16,15 @@ public class GildedRoseTest {
         assertEquals(9, item.quality);
         assertEquals(9,item.sellIn);
     }
-}
+
+    @Test
+    public void should_return_sellIn_is_9_quality_is_0_when_call_updateQuality_given_a_normal_item_with_sellIn_is_10_quality_is_0() {
+        Item item = new Item("Oreo", 10, 0);
+        Item[] items = {item};
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(0, item.quality);
+        assertEquals(9,item.sellIn);
+    }}

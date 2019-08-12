b66c81bb3e3788c75d310c266326ecdeec3f20dd
SamsonLin <153011490@qq.com>
2019-08-12 21:46:28 +0800

    test:add test given a Backstage item with sellIn is 10 and quality is 48 then return sellIn is 9 and quality is 50

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -173,5 +173,17 @@ public class GildedRoseTest {
         assertEquals(0,item.sellIn);
     }
 
+    @Test
+    public void should_return_sellIn_9_quality_50_when_call_updateQuality_given_a_Backstage_item_with_sellIn_10_quality_48() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 48);
+        Item[] items = {item};
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(50, item.quality);
+        assertEquals(9,item.sellIn);
+    }
+
 
 }

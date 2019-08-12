7c2dc93a3ffca1e669e70ce39a52f2d41d6998c8
SamsonLin <153011490@qq.com>
2019-08-12 21:05:33 +0800

    test:add test given a normal item with sellIn is -1 and quality is 0 then return sellIn is -2 and quality is 0

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -77,6 +77,18 @@ public class GildedRoseTest {
         assertEquals(1,item.sellIn);
     }
 
+    @Test
+    public void should_return_sellIn_minus_2_quality_is_0_when_call_updateQuality_given_a_normal_item_with_sellIn_minus_1_quality_is_0() {
+        Item item = new Item("Oreo", -1, 0);
+        Item[] items = {item};
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(0, item.quality);
+        assertEquals(-2,item.sellIn);
+    }
+
 
 
 

c353dbcdf2985821c5a0fe95620551e11c8eed73
SamsonLin <153011490@qq.com>
2019-08-12 22:09:16 +0800

    test:add test given a Aged Brie item with sellIn is 0 and quality is 50 then return sellIn is -1 and quality is 50

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -269,4 +269,16 @@ public class GildedRoseTest {
         assertEquals(0,item.sellIn);
     }
 
+    @Test
+    public void should_return_sellIn_minus_1_quality_50_when_call_updateQuality_given_a_Aged_Brie_item_with_sellIn_0_quality_50() {
+        Item item = new Item("Aged Brie", 0, 50);
+        Item[] items = {item};
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertEquals(50, item.quality);
+        assertEquals(-1,item.sellIn);
+    }
+
 }
